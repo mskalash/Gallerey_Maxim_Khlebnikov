6f912dab74cd7e54ca9af19f6022f92e6b6b7dd0
Maxim Khlebnikov <mskalash1@gmail.com>
2016-06-29 16:09:25 +0300

    Create GridView

---
+++ app/src/main/java/nan/gallerey_maxim_khlebnikov/ImageAdapter.java
@@ -0,0 +1,46 @@
+package nan.gallerey_maxim_khlebnikov;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    //Добавляем изображения в массив:
+    public Integer[] mThumbIds = {
+            R.drawable.leaves, R.drawable.sun, R.drawable.water,
+    };
+
+    //Конструктор:
+    public ImageAdapter(Context c){
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mThumbIds[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(70, 70));
+        return imageView;
+    }
+}
---
+++ app/src/main/java/nan/gallerey_maxim_khlebnikov/MainActivity.java
@@ -0,0 +1,39 @@
+package nan.gallerey_maxim_khlebnikov;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+
+        //Создаем пример класса ImageAdapter:
+        gridView.setAdapter(new ImageAdapter(this));
+
+        /**
+         * Обработчик нажатия для GridView элемента:
+         * */
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                //Отправляем id изображения в класс FullImageActivity:
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                //Передаем необходимый index:
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+    }
+}
