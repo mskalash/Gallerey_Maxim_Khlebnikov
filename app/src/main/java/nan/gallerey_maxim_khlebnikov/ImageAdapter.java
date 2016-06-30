199905ffb533c2be434c44924aa5208c6d3b495f
Maxim Khlebnikov <mskalash1@gmail.com>
2016-06-30 15:18:00 +0300

    add gallery telephone

--- app/src/main/java/nan/gallerey_maxim_khlebnikov/ImageAdapter.java
+++ app/src/main/java/nan/gallerey_maxim_khlebnikov/ImageAdapter.java
@@ -1,12 +1,22 @@
 package nan.gallerey_maxim_khlebnikov;
 
+import android.app.AlertDialog;
 import android.content.Context;
-import android.support.v4.view.ViewPager;
+
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
@@ -17,12 +27,13 @@ public class ImageAdapter extends BaseAdapter {
             R.drawable.e,R.drawable.e,R.drawable.e,R.drawable.e,R.drawable.e,R.drawable.e,R.drawable.e,
     };
 
+
     //Конструктор:
     public ImageAdapter(Context c){
         mContext = c;
     }
 
-    @Override
+   @Override
     public int getCount() {
         return mThumbIds.length;
     }
@@ -39,10 +50,16 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
+
         ImageView imageView = new ImageView(mContext);
+       /* Uri sourceUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+imageView.setImageURI(sourceUri);*/
+
         imageView.setImageResource(mThumbIds[position]);
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         imageView.setLayoutParams(new GridView.LayoutParams(190,  190));
         return imageView;
     }
+
 }
--- app/src/main/java/nan/gallerey_maxim_khlebnikov/MainActivity.java
+++ app/src/main/java/nan/gallerey_maxim_khlebnikov/MainActivity.java
@@ -1,39 +1,111 @@
 package nan.gallerey_maxim_khlebnikov;
-
-import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.CursorAdapter;
 import android.widget.GridView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity  implements LoaderCallbacks<Cursor>{
+
+    /** SimpleCursorAdapter, holds images and layout for the gridview */
+    SimpleCursorAdapter mAdapter;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        /** Initializes the Loader */
+        getSupportLoaderManager().initLoader(0, null, this);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /** Getting a reference to gridview of the MainActivity layout */
         GridView gridView = (GridView) findViewById(R.id.gridView);
 
-        //Создаем пример класса ImageAdapter:
-        gridView.setAdapter(new ImageAdapter(this));
+        /** Create an adapter for the gridview */
+        /** This adapter defines the data and the layout for the grid view */
+        mAdapter = new SimpleCursorAdapter(
+                getBaseContext(),
+                R.layout.gridview,
+                null,
+                new String[] { "_data","_id"} ,
+                new int[] { R.id.img},
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
+        );
 
-        /**
-         * Обработчик нажатия для GridView элемента:
-         * */
-        gridView.setOnItemClickListener(new OnItemClickListener() {
+        /** Setting adapter for the gridview */
+        gridView.setAdapter(mAdapter);
+
+        /** Loader to get images from the SD Card */
+        getSupportLoaderManager().initLoader(0, null, this);
+
+        /** Defining item click listener for the grid view */
+        OnItemClickListener itemClickListener = new OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                /** Getting the cursor object corresponds to the clicked item */
+                Cursor c1 = (Cursor ) arg0.getItemAtPosition(position);
 
-                //Отправляем id изображения в класс FullImageActivity:
+                /** Getting the image_id from the cursor */
+                /** image_id of the thumbnail is same as the original image id */
+                String id = c1.getString(c1.getColumnIndex("image_id"));
+
+                /** Creating a bundle object to pass the image_id to the ImageDialog */
+                Bundle b = new Bundle();
+
+                /** Storing image_id in the bundle object */
+                b.putString("image_id", id);
+
                 Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
                 //Передаем необходимый index:
-                i.putExtra("id", position);
+                i.putExtra("img",b);
+
                 startActivity(i);
+                return ;
             }
-        });
+
+        };
+
+        /** Setting itemclicklistener for the grid view */
+        gridView.setOnItemClickListener(itemClickListener);
+
+    }
+
+
+    /** A callback method invoked by the loader when initLoader() is called */
+    @Override
+    public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+        /** Getting uri to the Thumbnail images stored in the external storage */
+        Uri uri = MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI;
+
+        /** Invoking the uri */
+        return new CursorLoader(this, uri, null, null, null, null);
+    }
+
+    /** A callback method, invoked after the requested content provider returned all the data */
+    @Override
+    public void onLoadFinished(Loader<Cursor> arg0, Cursor arg1) {
+        mAdapter.swapCursor(arg1);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> arg0) {
+        // TODO Auto-generated method stub
     }
 }
+
+
